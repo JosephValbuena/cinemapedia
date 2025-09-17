4f47702dadc05229c8f200f857c9ac156cb81528
Josvalk <josvalk775@gmail.com>
2025-09-16 23:03:08 -0500

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: Color(0xFFFF9800)
+  );
+}
---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,1 @@
+export 'package:cinemapedia/presentation/screens/Movies/home_screen.dart';
